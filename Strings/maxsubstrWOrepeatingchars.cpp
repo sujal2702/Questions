bc968a9b246b36ae83d2991d27fc3a06ed6f5dbb
sujal2702 <sujalsingh2724@gmail.com>
2026-02-16 23:06:21 +0530

    set+brute force approach

--- Strings/maxsubstrWOrepeatingchars.cpp
+++ Strings/maxsubstrWOrepeatingchars.cpp
@@ -1,1 +1,37 @@
-// i will include a code here later with optimized approach
+// set + brute force approach 
+#include<bits/stdc++.h>
+#include<set>
+#include<string>
+using namespace std;
+
+
+    int lengthOfLongestSubstring(string s) {
+        int ans  = 0;
+        for(int i =0; i<s.size(); i++){
+            unordered_set <int> st;
+            for(int j=i; j<s.size(); j++){
+                if(st.count(s[j])){ //returns 0 if element is not present // returns 1 if element is present
+                    break;
+                }
+                else{
+                    st.insert(s[j]);
+                }
+                ans = max(ans , j-i+1); //j-i-1 length deta h ..changes for every i loop
+             }
+        }
+        return ans;
+    }
+
+int main(){
+    string input;
+    
+    cout << "Enter a string: ";
+    cin >> input;
+    
+    int result = lengthOfLongestSubstring(input);
+    
+    cout << "Input string: " << input << endl;
+    cout << "Length of longest substring without repeating characters: " << result << endl;
+    
+    return 0;
+}
