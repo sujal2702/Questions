7f5b458b245445f75bb3537d830585932d11e00d
Sujal <sujalsingh2724@gmail.com>
2026-02-21 16:43:20 +0530

    Add prime checking function in Maths.cpp
    Implement a function to check if a number is prime.

---
+++ Maths.cpp
@@ -0,0 +1,35 @@
+// to check whether a function is prime or not
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+        
+    if (n == 2)
+        return true;
+        
+    if (n % 2 == 0)
+        return false;
+
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    if (isPrime(n))
+        cout << n << " is Prime";
+    else
+        cout << n << " is Not Prime";
+
+    return 0;
+}
