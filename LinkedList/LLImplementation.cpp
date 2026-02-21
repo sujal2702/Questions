8bfb6286de8122cb790445202b6ff5cb32ec380c
sujal2702 <sujalsingh2724@gmail.com>
2026-02-22 00:28:18 +0530

    Insertion

--- LinkedList/LLImplementation.cpp
+++ LinkedList/LLImplementation.cpp
@@ -12,12 +12,44 @@ class Node{
         this->next = NULL;
     }
 };
+void InsertAtHead (Node* &head, int d){
+    // insert krne se pehle Node create to karo
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node* &head){
+
+    Node* temp = head;
+    while (temp != NULL){
+        cout << temp->data << " ";
+        temp = temp -> next;
+    }
+}
 
 int main(){
     Node* node1 = new Node(10);
     
     cout << "Data: " << node1->data << endl;
     cout << "Next pointer: " << node1->next << endl;
+
+    Node* head = node1; //head pointed to node 1;
+    print(head);
+    cout << endl;
+    
+    InsertAtHead(head , 12);
+    InsertAtHead(head , 15);
+    print(head);
+    cout << endl;
+    
+
     
     return 0;
 }
+
+//While Printing 
+
+    //node already h  sirf print krna h to No Creation
+    //since head ko move ya change nhi kr skte while printing LL disturb ho jayega
+    //That's Why head pe bhi ek pointer laga do - temp naam ka
