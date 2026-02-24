c367f3203633ea6bf5c03c3389b5737a6208195e
sujal2702 <sujalsingh2724@gmail.com>
2026-02-25 02:03:39 +0530

    Function to Convert Array Into Linked List

--- LinkedList/BasicsLL.cpp
+++ LinkedList/BasicsLL.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 //Converting from Linked List to array
 
-vector<int> linkedListToArray(Node* head) {
+vector<int> linkedListToArray(ListNode* head) {
 
     vector<int> arr;
     ListNode* temp = head;
@@ -15,3 +15,24 @@ vector<int> linkedListToArray(Node* head) {
 
     return arr;
 }
+
+//Convert Array Into Linked List
+
+ListNode* arrayToLinkedList(vector<int>& arr) {
+
+    if(arr.size() == 0) return NULL;
+
+    // Create head
+    ListNode* head = new ListNode(arr[0]); //pehle 1st element create kar lo 
+    ListNode* curr = head;                // aur head point kar do uspe
+
+    for(int i = 1; i < arr.size(); i++){
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+
+
