6cc0380ae42e64654bb88b4a21665ae31d0ee549
sujal2702 <sujalsingh2724@gmail.com>
2026-03-01 23:26:14 +0530

    Finding element

--- Arrays/Basic.cpp
+++ Arrays/Basic.cpp
@@ -45,3 +45,21 @@ int main() {
 
     return 0;
 }
+
+int findPeak(int arr[], int n) {
+
+    int s =0, e = n-1;
+    int mid = s + (e-s)/2;
+
+    while(s<e) {
+        //cout<<" s " << s <<" e " << e << endl;
+        if(arr[mid] < arr[mid+1]){
+            s = mid+1; 
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return s;
+}
