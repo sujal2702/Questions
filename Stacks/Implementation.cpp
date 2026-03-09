33de9021b9c10823382452358d688028ac5989f7
sujal2702 <sujalsingh2724@gmail.com>
2026-03-10 01:28:46 +0530

    Implementation of stack using array

--- Stacks/Implementation.cpp
+++ Stacks/Implementation.cpp
@@ -1,63 +1,144 @@
-// Stacks STL FUNCTION
+//Stack Implementation using array
 
-#include <iostream>
-#include <stack>
+#include<iostream>
+#include<stack>
 using namespace std;
 
-int main() {
+class Stack{
+    public:
+    
+    int size;
+    int *arr;
+    int top;
 
-    // Creating a stack
-    stack<int> s;
-
-    // PUSH - inserting elements
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-
-    cout << "After pushing elements:\n";
-    cout << "Top element: " << s.top() << endl;
-    cout << "Size: " << s.size() << endl;
-
-    // POP - removing top element
-    s.pop();
-
-    cout << "\nAfter one pop:\n";
-    cout << "Top element: " << s.top() << endl;
-    cout << "Size: " << s.size() << endl;
-
-    // Checking empty
-    if(s.empty())
-        cout << "\nStack is empty\n";
-    else
-        cout << "\nStack is NOT empty\n";
-
-    // Traversing stack (by copying)
-    stack<int> temp = s;
-
-    cout << "\nStack elements (top to bottom): ";
-    while(!temp.empty()) {
-        cout << temp.top() << " ";
-        temp.pop();
+    Stack(int size){ //constructor
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
     }
 
-    // SWAP example
-    stack<int> s2;
-    s2.push(100);
-    s2.push(200);
+    void push(int element){
+        if(size - top > 1) {//logic to check if stack is empty or not
+            // if true means not empty
+            top++;
+            arr[top] = element;
+        }
+        else{
+            cout <<"Stack Overflow" << endl;
+        }
+    }
 
-    cout << "\n\nBefore swap:";
-    cout << "\nStack1 size: " << s.size();
-    cout << "\nStack2 size: " << s2.size();
+    void pop(){
+        //first check if stack is not empty
+        if(top >= 0){
+            top--;
+        }
+        else{
+            cout <<" Stack Underflow" << endl;
+        }
+    }
 
-    s.swap(s2);
+    int peek(){
+        if(top >= 0) return arr[top]; // we can't return just top because we need to return int not pointer
+        else{
+            cout<<"Stack is Empty so not top to return " <<endl;
+            return -1;
+        }
+    }
 
-    cout << "\n\nAfter swap:";
-    cout << "\nStack1 size: " << s.size();
-    cout << "\nStack2 size: " << s2.size();
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
 
-    return 0;
+int main(){
+
+    Stack st(5);
+
+    st.push(5);
+    st.push(10);
+    st.push(20);
+    st.push(5);
+    st.push(10);
+    st.push(20); // this will create stack overflow condition
+
+    cout <<"Top element of stack is " <<st.peek() <<endl;
+
+    st.pop();
+
+    cout <<"Top element of stack is " <<st.peek() <<endl;
+
+    st.pop();
+    st.pop();
+
+    cout <<"Top element of stack is " <<st.peek() <<endl;
 }
+
+// Stacks STL FUNCTION
+
+// #include <iostream>
+// #include <stack>
+// using namespace std;
+
+// int main() {
+
+//     // Creating a stack
+//     stack<int> s;
+
+//     // PUSH - inserting elements
+//     s.push(10);
+//     s.push(20);
+//     s.push(30);
+//     s.push(40);
+
+//     cout << "After pushing elements:\n";
+//     cout << "Top element: " << s.top() << endl;
+//     cout << "Size: " << s.size() << endl;
+
+//     // POP - removing top element
+//     s.pop();
+
+//     cout << "\nAfter one pop:\n";
+//     cout << "Top element: " << s.top() << endl;
+//     cout << "Size: " << s.size() << endl;
+
+//     // Checking empty
+//     if(s.empty())
+//         cout << "\nStack is empty\n";
+//     else
+//         cout << "\nStack is NOT empty\n";
+
+//     // Traversing stack (by copying)
+//     stack<int> temp = s;
+
+//     cout << "\nStack elements (top to bottom): ";
+//     while(!temp.empty()) {
+//         cout << temp.top() << " ";
+//         temp.pop();
+//     }
+
+//     // SWAP example
+//     stack<int> s2;
+//     s2.push(100);
+//     s2.push(200);
+
+//     cout << "\n\nBefore swap:";
+//     cout << "\nStack1 size: " << s.size();
+//     cout << "\nStack2 size: " << s2.size();
+
+//     s.swap(s2);
+
+//     cout << "\n\nAfter swap:";
+//     cout << "\nStack1 size: " << s.size();
+//     cout << "\nStack2 size: " << s2.size();
+
+//     return 0;
+// }
 //--------------------------------------------------------------------------------------------------------------------------
 //OUTPUT
 /* 
