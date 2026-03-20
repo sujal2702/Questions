f7b8eea7330235454d10566b68f825b69e3a233b
Sujal <sujalsingh2724@gmail.com>
2026-03-20 22:55:07 +0530

    Conversion algorithms
    Added details for infix to postfix and prefix conversion logic.

--- Stacks/conversion.cpp
+++ Stacks/conversion.cpp
@@ -66,4 +66,99 @@ class Solution {
     }
 };
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/* --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Infix to postfix conversion
+
+ Rules ( IMPORTANT)
+1. Operand (a, b, 1, 2…)
+
+➡️ Directly add to answer string
+
+2. (
+
+➡️ Push to stack
+
+3. )
+
+➡️ Pop until '(' is found
+
+4. Operator (+ - * / ^)
+
+➡️ While stack has higher/equal priority → pop
+➡️ Then push current operator
+
+⚠️ Exception:
+
+^ → right associative (don’t pop on equal)
+
+5. End of expression
+
+-----------PSEUDOCODE-----------------------
+
+function priority(op):
+
+    if op == '+' or '-': return 1
+    if op == '*' or '/': return 2
+    if op == '^': return 3
+
+    return 0
+function infixToPostfix(s):
+
+    create empty stack st
+    create empty string ans
+
+    for each character ch in s:
+
+        if ch is operand:
+            append ch to ans
+
+        else if ch == '(':
+            push ch to stack
+
+        else if ch == ')':
+            while stack is not empty AND top != '(':
+                append top to ans
+                pop stack
+            pop '(' from stack
+
+        else:  // operator
+
+            while stack is not empty AND
+                  (priority(ch) < priority(top) OR
+                  (priority(ch) == priority(top) AND ch != '^')):
+
+                append top to ans
+                pop stack
+
+            push ch to stack
+
+    // after traversal
+    while stack is not empty:
+        append top to ans
+        pop stack
+
+    return ans
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+Infix to Prefix Conversion
+
+1. Reverse the string + after reversing completely reverse the bracket
+2. Perform this infix to postfix conversion and store it in answer 
+3. At last reverse the answer
+
+one change in logic of infix to postfix logic here
+
+If current character is operator:
+
+    If operator == '^':
+        While stack not empty AND
+              priority(curr) <= priority(stack.top()):
+            pop → add to answer
+
+    Else:
+        While stack not empty AND
+              priority(curr) < priority(stack.top()):
+            pop → add to answer
+
+    Push current operator to stack
+    
+ */
