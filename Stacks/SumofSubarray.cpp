fb8799016dcc7255d642cb124f19b4be36c8f222
Sujal <sujalsingh2724@gmail.com>
2026-03-27 23:52:18 +0530

    Add brute force solution for sum of subarray minimums
    Implemented a brute force approach to calculate the sum of minimums of all subarrays.

--- Stacks/SumofSubarray.cpp
+++ Stacks/SumofSubarray.cpp
@@ -1,1 +1,16 @@
-//To find sum of subarray minimum 
+//To find Sum of Subarray Minimum 
+//--------------------------->>BRUTE FORCE APPROACH------------------------------- T.C.O(n^2)
+class Solution {
+  public:
+    int sumSubMins(vector<int> &arr) {
+        int sum = 0;
+        for(int i=0; i<arr.size(); i++){
+            int mini = INT_MAX;
+            for(int j=i; j<arr.size(); j++){ //Iterate through every possible subarray combination and store min & add it in sum.
+                mini = min(mini , arr[j]);
+                sum = sum + mini;
+            }
+        }
+        return sum;
+    }
+};
