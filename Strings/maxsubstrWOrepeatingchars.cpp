a2ec6327582dbeafdbcb6a47d4d561b0c2a58ef1
Sujal <sujalsingh2724@gmail.com>
2026-05-02 16:31:50 +0530

    Add Sliding Window approach for longest substring length
    Implemented the Sliding Window and Two Pointer approach to find the length of the longest substring without repeating characters in O(N) time complexity.

--- Strings/maxsubstrWOrepeatingchars.cpp
+++ Strings/maxsubstrWOrepeatingchars.cpp
@@ -21,6 +21,35 @@ using namespace std;
         }
         return ans;
     }
+// this is of Time complexity O(N^2)
+
+//The Sliding Window + Two Pointer Approach in O(N) Time Complexity
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+        int hash[256];
+        fill(hash, hash + 256, -1);
+        while (r < n) {
+            // check if the element is in map or not
+
+            if (hash[s[r]] != -1) { // this means it is in the map
+
+                if (hash[s[r]] >= l) { //jo last occurrence hai, kya wo current window ke andar hai?
+                    l = hash[s[r]] + 1; //shrink window where there in no duplicate
+                }
+            }
+            // if it is not in  the map
+            maxLen = max(maxLen, r - l + 1);
+            hash[s[r]] = r;
+            r++;
+        }
+        return maxLen;
+    }
+};
 
 int main(){
     string input;
