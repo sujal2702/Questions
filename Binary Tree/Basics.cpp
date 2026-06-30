962ce4a9d96efab2ce7deb88722177e5f6cd4549
Sujal <sujalsingh2724@gmail.com>
2026-06-30 23:53:01 +0530

    Height of a tree

--- Binary Tree/Basics.cpp
+++ Binary Tree/Basics.cpp
@@ -1,1 +1,34 @@
 // Easy level Questions of Binary Tree
+/*
+ With Respect to this given function
+ Definition for Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+//Height of a Tree 
+
+class Solution {
+  public:
+    int height(Node* root) {
+      
+        if(root == NULL){
+            return 0;
+        }
+        
+        int left = height(root->left);
+        int right = height(root->right);
+        
+        int ans = max(left , right) + 1;
+        return ans;
+    }
+};
+
