b700681b04d74ed33a511eeb05bd8a6324b65983
Sujal <sujalsingh2724@gmail.com>
2026-07-01 23:49:50 +0530

    Diamtere of a tree with height function

--- Binary Tree/Basics.cpp
+++ Binary Tree/Basics.cpp
@@ -32,3 +32,39 @@ class Solution {
     }
 };
 
+//Diameter of a tree with height // T.C. O(N^2)
+
+class Solution {
+  public:
+  int height(Node* root) {
+        // code here
+        if(root == NULL){
+            return -1;
+        }
+        
+        int left = height(root->left);
+        int right = height(root->right);
+        
+        // ab ye wala part return aate wakt dekha jata h
+        int ans = max(left,right)+1;
+        return ans;
+    }
+    
+    int diameter(Node* root) {
+        // code here
+        if(root == NULL){
+            return 0;
+        }
+        
+        int leftpart1 = height(root->left);
+        int rightpart1 = height(root->right);
+        int mid = height(root->left) + height(root->right)+2;
+        
+        int leftpart = diameter(root->left);
+        int rightpart = diameter(root->right);
+        
+        int dia = max(leftpart ,max(rightpart , mid));
+        return dia;
+    }
+};
+
