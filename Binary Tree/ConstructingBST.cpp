513a6f83e62f9b7483604fec9418f0970e32da48
Sujal <sujalsingh2724@gmail.com>
2026-07-09 01:52:20 +0530

    postorder to BST but not Optimal
    postorder hai but nomal insertion in BST wala logic use hua h REAL solution ye nhi h

--- Binary Tree/ConstructingBST.cpp
+++ Binary Tree/ConstructingBST.cpp
@@ -1,2 +1,36 @@
 //CONSTRUCTING BST FROM DIFFERNT WAYS --> FROM INORDER , PREORDER AND POSTORDER TRAVERSAL
 
+//BST CONSTRUCTION USING POSTORDER TRAVERSAL
+
+class Solution {
+  public:
+    Node* insertIntoBst(Node* root, int val){
+        
+        if(root == NULL){
+            root = new Node(val);
+            return root;
+        }
+        
+        if(val < root->data){
+            root->left = insertIntoBst(root->left,val);
+        }
+        else{
+            root->right = insertIntoBst(root->right,val);
+        }
+        return root;
+    }
+    Node* constructTree(vector<int>& post) {
+        
+        Node* root = NULL;
+        
+        int n = post.size();
+        
+        for(int i=n-1; i>=0; i--){
+            root = insertIntoBst(root,post[i]);
+        }
+        
+        return root;
+    }
+};
+
+// POSTORDER ME ROOT LAST ME HOTA H ISLIYE PICHE SE VALUE INSERTION START KIYE H
