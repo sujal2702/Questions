19e2e3fb4c423b7e79b6b704a8d1b36076acea0d
Sujal <sujalsingh2724@gmail.com>
2026-08-14 23:46:04 +0530

    Non OverLapping Interval
    Implement a solution to erase overlapping intervals by sorting and counting non-overlapping ones.

--- Greedy/Non_Overlapping.cpp
+++ Greedy/Non_Overlapping.cpp
@@ -1,1 +1,26 @@
+//Non Overlapping Interval
 
+bool comp(vector<int>& a, vector<int>& b) {
+    return a[1] < b[1];
+    }
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), comp);
+
+        int lastSeen = intervals[0][1];
+        int count = 1;
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            if (intervals[i][0] >= lastSeen) {
+                count++;
+                // iske baad aage badhega i therefore
+                lastSeen = intervals[i][1];
+            }
+        }
+
+        return intervals.size() - count;
+    }
+};
