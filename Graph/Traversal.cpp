5e6bd23402ca97fe491bc03c003b1fb4f8981b67
Sujal <sujalsingh2724@gmail.com>
2026-08-21 14:57:10 +0530

    Add BFS traversal method
    Implement BFS traversal for a graph using a queue.

--- Graph/Traversal.cpp
+++ Graph/Traversal.cpp
@@ -1,1 +1,34 @@
+//BFS TRAVERSAL OF A GRAPH
+
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+       // pehle visited array bana lo {0} , starting node ka visited = 1 mark kar do
+      // phir Queue banao --> 1st node push karo, usse node me dalo , phir pop karo, phir vector<int> me dalo
+     // uske baad Iterate kro --> ajd[nodes] k har element ko queue me push karo --> ye sab krte rho jab tak queue empty nhi hota
+        
+        int n = adj.size(); //equals to the size of nodes
+        vector<int>bfs;
+        
+        int visited[n] = {0}; 
+        visited[0] = 1;
+        queue<int>q;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            bfs.push_back(node);
+            
+            for(auto it : adj[node]){ //in 1st case for every element of adj[0] 
+                if(visited[it] != 1){
+                    visited[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
 
