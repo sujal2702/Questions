fa3adecde080e5319b07d0ea80b09d2642615fb6
Sujal <sujalsingh2724@gmail.com>
2026-09-10 01:01:10 +0530

    Implemented common dfs method (generally used one)

--- Graph/Traversal.cpp
+++ Graph/Traversal.cpp
@@ -62,3 +62,69 @@ class Solution {
         return ls;
     }
 };
+
+// ----MAXIMUM ISS WAY SE TRAVERSE KAR RHE HOTE H GRAPH KO -------------------------------- JUST SEE THE WAY AND CHNAGE THE CONDITIONS ACCORDING TO THE QUESTION----------------------------------------------------
+class Solution {
+private:
+void dfs(int row, int col, vector<vector<int>>&vis,vector<vector<int>>& grid, int delRow[], int delCol[]){
+
+    int n = grid.size();
+    int m = grid[0].size();
+
+    vis[row][col] = 1;
+        
+    for(int i=0; i<4; i++){
+
+        int nrow = row + delRow[i];
+        int ncol = col + delCol[i];
+
+        if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m &&
+        !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+
+            dfs(nrow, ncol, vis, grid, delRow, delCol);
+        }
+    }
+}
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        vector<vector<int>>vis(n, vector<int>(m,0));
+
+        for(int j=0; j<m; j++){ //0 and last row me sara column traverse karo
+            if(grid[0][j] == 1 && !vis[0][j]){
+                dfs(0,j, vis, grid, delRow, delCol);
+            }
+
+            if(grid[n-1][j] == 1 && !vis[n-1][j]){
+                dfs(n-1, j, vis, grid, delRow, delCol);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(grid[i][0] == 1 && !vis[i][0]){
+                dfs(i, 0, vis, grid, delRow, delCol);
+            }
+
+             if(grid[i][m-1] == 1 && !vis[i][m-1]){
+                dfs(i, m-1, vis, grid, delRow, delCol);
+            }
+        }
+        int count = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!vis[i][j] && grid[i][j] == 1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//
