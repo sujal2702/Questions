d0ac639c39dd28faec673567053007e5d6f72e2c
Sujal <sujalsingh2724@gmail.com>
2026-09-10 01:10:11 +0530

    Implemented most commonly used BFS Technique

--- Graph/Traversal.cpp
+++ Graph/Traversal.cpp
@@ -127,4 +127,58 @@ public:
     }
 };
 
-//
+// AUR YE WALA MAXIMUM USED BFS TECHNIQUE ISME SIRF CONDITION (Number of enclave question k according h ) BUT BFS TRAVERSAL NORMALLY JO USE HOTA H WAHI H -------------------------------------------------------
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        vector<vector<int>>vis(n, vector<int>(m,0));
+
+        queue<pair<int,int>>q;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i==0 || j==0 || i == n-1 || j == m-1){
+                    if(grid[i][j] == 1){
+                        q.push({i,j});
+                        vis[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        while(!q.empty()){
+
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++){
+
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if(nrow >= 0 && nrow <n && ncol >= 0 && ncol < m &&
+                !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol] = 1;
+                } 
+            }
+        }
+
+        int count = 0;
+         for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!vis[i][j] && grid[i][j] == 1) count++;
+            }
+        }
+        return count;
+    }
+};
